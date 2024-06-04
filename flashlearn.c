cebad1864eeb62b169bf7375e9d52d80e47a9ada
LuckyToast <LuckyToast@github.com>
2024-06-05 01:29:37 +0200

    added way of adding bundles through tokenizable files

--- flashlearn.c
+++ flashlearn.c
@@ -7,43 +7,42 @@
 #define MAX_WORDS 50
 #define WORD_LEN 50
 
-typedef struct {
+typedef struct
+{
     char name[WORD_LEN];
     char german[MAX_WORDS][WORD_LEN];
     char english[MAX_WORDS][WORD_LEN];
     int word_count;
 } bundle_t;
 
-
 // main functions
 bundle_t* new_bundle();
 bundle_t* load_bundles(FILE* f);
-void save_bundle(FILE* f);
-void print_bundles(bundle_t* bundles, size_t size);
+bundle_t* new_from_file(FILE* f, char* name);
+void save_bundle(FILE *f);
+void print_bundles(bundle_t *bundles, size_t size);
 void play();
 
 // helper functions
 void clear_stdin();
-void trim(char* str);
-void get_input_int(int* dst, char* msg);
-void get_input_str(char* dst, size_t dst_len, char* msg);
-FILE* open_file(const char* path, char* mode);
-size_t file_size(FILE* f);
-
+void trim(char *str);
+void get_input_int(int *dst, char *msg);
+void get_input_str(char *dst, size_t dst_len, char *msg);
+FILE* open_file(const char *path, char *mode);
+size_t file_size(FILE *f);
 
 const char CONFIG_PATH[] = "flashlearn.bin";
 const char NAME[] = "NAME\n\tFLASHLEARN - make bundles of flashcards and play a word memorisation game\n";
 const char SYNOPSIS[] = "SYNOPSIS\n\tflashlang [OPTION]\n";
 const char DESCRIPTION[] = "DESCRIPTION:\n\t-p, --print\tPrint all the bundles\n\t -h, --help\tPrint this menu\n\t-n, --new\tMake new word bundle\n";
 
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc > 2) {
         printf("%s", DESCRIPTION);
         return 1;
-    } 
+    }
 
-    FILE* f = open_file("flashlearn.bin", "ab+");
+    FILE* f = open_file(CONFIG_PATH, "ab+");
     if (file_size(f) < sizeof(bundle_t) && argc == 1) {
         printf("Config File '%s' appears empty\n", CONFIG_PATH);
         exit(1);
@@ -52,11 +51,31 @@ int main(int argc, char** argv) {
     bundle_t* bundles = load_bundles(f);
     size_t bundle_n = file_size(f) / sizeof(bundle_t);
 
-    //FILE* f2 = open_file("flashlearn.bin.3", "ab");
     
-    //for (int i = 0; i < bundle_n - 1; i++) 
-        //fwrite(&bundles[i], sizeof(bundle_t), 1, f2);
-    
+    FILE* f2 = open_file("verbs2", "rb");
+    bundle_t *v2 = new_from_file(f2, "verbs 2");
+    fclose(f2);
+
+    FILE *f3 = open_file("verbs3", "rb");
+    bundle_t *v3 = new_from_file(f3, "verbs 3");
+    fclose(f3);
+
+    FILE *f4 = open_file("verbs4", "rb");
+    bundle_t *v4 = new_from_file(f4, "verbs 4");
+    fclose(f4);
+
+    print_bundles(v2, 1);
+    print_bundles(v3, 1);
+    print_bundles(v4, 1);
+
+    fwrite(v2, sizeof(bundle_t), 1, f);
+    fwrite(v3, sizeof(bundle_t), 1, f);
+    fwrite(v4, sizeof(bundle_t), 1, f);
+
+    free(v2);
+    free(v3);
+    free(v4);
+
     if (argc == 1) play(bundles, bundle_n);
 
     if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
@@ -77,8 +96,30 @@ int main(int argc, char** argv) {
 }
 
 
-bundle_t* new_bundle() {
+bundle_t *new_from_file(FILE *f, char* name) {
     bundle_t* b = (bundle_t*) malloc(sizeof(bundle_t));
+    strcpy(b->name, name);
+
+    int i = 0, e = 0;
+    char line[100];
+    while (fgets(line, sizeof(line), f) != NULL) {
+        char* token = strtok(line, "-");
+        while (token != NULL) {
+            trim(token); 
+            if (e % 2 == 0) strcpy(b->english[i], token);
+            else strcpy(b->german[i], token);
+            token = strtok(NULL, "-");
+            e++;
+        } 
+        i++;
+    }
+    b->word_count = i;
+    return b;
+}
+
+
+bundle_t *new_bundle() {
+    bundle_t *b = (bundle_t *)malloc(sizeof(bundle_t));
     get_input_str(b->name, WORD_LEN, "Bundle name? >> ");
     get_input_int(&b->word_count, "Number of words? >> ");
 
@@ -104,38 +145,33 @@ bundle_t* new_bundle() {
     return b;
 }
 
-
-bundle_t* load_bundles(FILE* f) {
+bundle_t *load_bundles(FILE *f) {
     size_t bundle_n = file_size(f) / sizeof(bundle_t);
-    bundle_t* bundles = (bundle_t*) malloc(sizeof(bundle_t) * bundle_n);
-    fread(bundles, sizeof(bundle_t), bundle_n , f);
+    bundle_t *bundles = (bundle_t *)malloc(sizeof(bundle_t) * bundle_n);
+    fread(bundles, sizeof(bundle_t), bundle_n, f);
     return bundles;
 }
 
-
-void get_input_str(char* dst, size_t dst_len, char* msg) {
+void get_input_str(char *dst, size_t dst_len, char *msg) {
     printf("%s", msg);
     fgets(dst, dst_len, stdin);
     trim(dst);
 }
 
-
-void get_input_int(int* dst, char* msg) {
+void get_input_int(int *dst, char *msg) {
     printf("%s", msg);
     scanf("%d", dst);
     clear_stdin();
 }
 
-
 void clear_stdin() {
-    int c; 
+    int c;
     while ((c = getchar()) != '\n');
 }
 
-
-void trim(char* str) {
-    char* start = str;
-    char* end = str + strlen(str) - 1;
+void trim(char *str) {
+    char *start = str;
+    char *end = str + strlen(str) - 1;
 
     while (*start == ' ' || *start == '\n') start++;
     while (end > start && (*end == ' ' || *end == '\n')) end--;
@@ -144,26 +180,24 @@ void trim(char* str) {
     if (start != str) memmove(str, start, end - start + 2);
 }
 
-
-FILE* open_file(const char* path, char* mode) {
-    FILE* f = fopen(path, mode);
-    if (f == NULL) {
+FILE *open_file(const char *path, char *mode) {
+    FILE *f = fopen(path, mode);
+    if (f == NULL)
+    {
         perror("Couldn't open config file");
-        exit(EXIT_FAILURE); 
+        exit(EXIT_FAILURE);
     }
     return f;
 }
 
-
-size_t file_size(FILE* f) {
+size_t file_size(FILE *f) {
     fseek(f, 0, SEEK_END);
     size_t res = ftell(f);
     rewind(f);
     return res;
 }
 
-
-void print_bundles(bundle_t* bundles, size_t size) {
+void print_bundles(bundle_t *bundles, size_t size) {
     for (int i = 0; i < size; i++) {
         printf("%s:\n", bundles[i].name);
         for (int k = 0; k < bundles[i].word_count; k++)
@@ -172,8 +206,7 @@ void print_bundles(bundle_t* bundles, size_t size) {
     }
 }
 
-
-void play(bundle_t* bundles, size_t size) {
+void play(bundle_t *bundles, size_t size) {
     srand(time(NULL));
 
     for (int i = 0; i < 3; i++) {
@@ -196,4 +229,3 @@ void play(bundle_t* bundles, size_t size) {
         } printf("Next ... Boonda!\n");
     } printf("YOU DIED. WOMP WOMP! :()\n");
 }
-
